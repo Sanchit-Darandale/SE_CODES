8ab3dccbcf9828723ba950aa25e7e5b320c6b138
Sanchit Darandale <firstlysanchit@gmail.com>
2025-07-09 15:36:23 +0530

    Add files via upload

---
+++ Pycodes/bfs_01.py
@@ -0,0 +1,25 @@
+
+visited = []
+queue = []
+graph = {
+    "5": ["3", "7"],
+    "3": ["2", "4"],
+    "7": ["8"],
+    "2": [],
+    "4": ["8"],
+    "8": []
+}
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+    while queue:
+        m = queue.pop(0)
+        print(m, end=" ")
+        for neighbor in graph[m]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+    print("\nBFS complete")
+
+bfs(visited, graph, "5")
